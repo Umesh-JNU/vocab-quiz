88fe756aed9aac5732859f57a31f4ccb89a1ecb4
Umesh Kumar <umesh98_soe@jnu.ac.in>
2023-04-06 15:31:53 +0530

    styling

--- components/Container.js
+++ components/Container.js
@@ -1,15 +1,23 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+// demonstate different ways of using styling
+// First import the stylesheet
+import ExStyle from "../styles/style";
 
 const Container = ({ child }) => {
-  return <View style={styles.container}>{child}</View>;
+  return (
+    <View
+      style={[styles.container, ExStyle.bg, { padding: 16 }]}
+    >
+      {child}
+    </View>
+  );
 };
 
 export default Container;
 
 const styles = StyleSheet.create({
   container: {
-    padding: 16,
-    height: '100%'
+    height: "100%",
   },
 });
---
+++ styles/style.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+const ExStyle = StyleSheet.create({
+  bg: {
+    backgroundColor: "rgb(200,223,255)",
+  },
+});
+
+export default ExStyle;
